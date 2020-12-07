9703a119e2f6c86c0de77774be04994b38a54d8e
wellinthatcase <63752837+wellinthatcase@users.noreply.github.com>
2020-12-06 22:56:47 -0500

    Port to C++/CLI

--- src/Lively.cpp
+++ src/Lively.cpp
@@ -1,91 +1,12 @@
-﻿#include "Lively.h"
+#include "Lively.h"
 
-INT WINAPI WinMain(
-	_In_     HINSTANCE hInstance,
-	_In_opt_ HINSTANCE prevhInst,
-	_In_     LPSTR     pCmdLine,
-	_In_     INT       nCmdShow
-)
-{
-	UNREFERENCED_PARAMETER(pCmdLine);
-	UNREFERENCED_PARAMETER(prevhInst);
-	 
-	WNDCLASS wc = { };
-	
-	wc.hInstance     = hInstance;
-	wc.lpfnWndProc   = WindowProcedure;
+[System::STAThread]
+void LivelyMain(array<System::String^>^ arguments) {
+	using Application = System::Windows::Forms::Application;
 
-	wc.hIcon         = WINDOW_ICONS;
-	wc.style         = WINDOW_STYLE;
-	wc.hCursor       = WINDOW_CURSR;
-	wc.cbClsExtra    = EXTRA_BYTES;
-	wc.cbWndExtra    = EXTRA_BYTES;
-	wc.lpszClassName = WINDOW_CNAME;
-	wc.hbrBackground = WINDOW_BRUSH;
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false);
 
-	RegisterClassW(&wc);
-
-	HWND hwnd = CreateWindowEx(
-		NULL,
-		WINDOW_CNAME,
-		WINDOW_CNAME,
-		WINDOW_FLAGS,
-		CW_USEDEFAULT,
-		CW_USEDEFAULT,
-		PX_WIN_WIDTH,
-		PX_WIN_HEIGHT,
-		NULL,
-		NULL,
-		hInstance,
-		NULL
-	);
-
-	if (hwnd == NULL)
-		return NULL;
-
-	ShowWindow(hwnd, nCmdShow);
-
-	MSG message = { };
-	while (GetMessage(&message, hwnd, WMSG_MIN, WMSG_MAX))
-	{
-		TranslateMessage(&message);
-		DispatchMessageW(&message);
-		Sleep(COOLDOWN_MS);
-	}
-
-	return message.wParam ? TRUE : FALSE;
-}
-
-LRESULT CALLBACK WindowProcedure(
-	_In_ HWND   hwnd,
-	_In_ UINT   uMsg,
-	_In_ WPARAM WPrm,
-	_In_ LPARAM LPrm
-)
-{
-	switch (uMsg)
-	{
-	case WM_CLOSE:
-		if (MessageBox(hwnd, L"Are you sure you wanna quit?", L"Confirm", MB_YESNO) == IDYES) {
-			DestroyWindow(hwnd);
-		}
-		return FALSE;
-
-	case WM_DESTROY:
-		PostQuitMessage(0);
-		return FALSE;
-
-	case WM_PAINT:
-	{
-		PAINTSTRUCT paint = { };
-		HDC hdc = BeginPaint(hwnd, &paint);
-
-		FillRect(hdc, &paint.rcPaint, WINDOW_BRUSH);
-		EndPaint(hwnd, &paint);
-	}
-	return FALSE;
-
-	default:
-		return DefWindowProc(hwnd, uMsg, WPrm, LPrm);
-	}
+	Lively::Lively window = { };
+	Application::Run(% window);
 }
