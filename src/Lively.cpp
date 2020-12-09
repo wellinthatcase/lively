7ccf0f68ba137a50863f5750d3844c3da214426e
wellinthatcase <63752837+wellinthatcase@users.noreply.github.com>
2020-12-09 18:58:05 -0500

    Add copyright boilerplate

--- src/Lively.cpp
+++ src/Lively.cpp
@@ -1,7 +1,11 @@
+// ----------------------------------------------- //
+//		    © 2020 - All rights reserved.          // 
+// ----------------------------------------------- //
+
 #include "Lively.h"
 
 [System::STAThread]
-void LivelyMain(array<System::String^>^ arguments) {
+void LivelyMain(_In_opt_ array<System::String^>^ arguments) {
 	using Application = System::Windows::Forms::Application;
 
 	Application::EnableVisualStyles();
