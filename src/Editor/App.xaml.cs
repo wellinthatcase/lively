eab487ea9c78ddf63c84a818768785b7862a8d9e
wellinthatcase <63752837+wellinthatcase@users.noreply.github.com>
2020-12-20 21:17:43 -0500

    Create App.xaml.cs

---
+++ src/Editor/App.xaml.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Editor
+{
+    public partial class Application : System.Windows.Application {}
+}
