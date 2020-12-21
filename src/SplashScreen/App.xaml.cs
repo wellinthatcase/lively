384aff1384f73d3432ddec9c1e8a90f9dae41add
wellinthatcase <63752837+wellinthatcase@users.noreply.github.com>
2020-12-20 21:15:27 -0500

    Create App.xaml.cs

---
+++ src/SplashScreen/App.xaml.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Data;
+using System.Linq;
+using System.Windows;
+using System.Configuration;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace Lively
+{
+    public partial class App : Application {}
+}
