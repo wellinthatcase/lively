76c0f62ac6b9aa06be69e7626ffd57b4ea36c7ac
wellinthatcase <63752837+wellinthatcase@users.noreply.github.com>
2021-01-05 15:46:08 -0500

    Update copyright year

--- src/SplashScreen/MainWindow.xaml.cs
+++ src/SplashScreen/MainWindow.xaml.cs
@@ -1,22 +1,13 @@
 // =============================================== //
-//		    © 2020 - All rights reserved.          // 
+//		    © 2021 - All rights reserved.          // 
 // =============================================== //
 
 using System;
 using System.Linq;
-using System.Text;
 using System.Windows;
 using System.Diagnostics;
-using System.Windows.Data;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Shapes;
-using System.Threading.Tasks;
 using System.Windows.Controls;
-using System.Windows.Documents;
-using System.Windows.Navigation;
-using System.Collections.Generic;
-using System.Windows.Media.Imaging;
 
 namespace Lively
 {
